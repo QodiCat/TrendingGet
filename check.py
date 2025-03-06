df88165c07b54447195f18d914e485a584f82d66
Qodi <1357016290@qq.com>
2025-03-06 13:36:13 +0800

    fix:修复无法远端上传的问题

--- check.py
+++ check.py
@@ -27,7 +27,7 @@ def load_config():
 def git_add_commit_push(date, filename):
     cmd_git_add = 'git add {filename}'.format(filename=filename)
     cmd_git_commit = 'git commit -m "{date}"'.format(date=date)
-    cmd_git_push = 'git push -u origin master'
+    cmd_git_push = 'git push -u origin main'
 
     os.system(cmd_git_add)
     os.system(cmd_git_commit)
